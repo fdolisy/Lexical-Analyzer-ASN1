082cfd5b76151c8d8037ccd76cfdf2ad252a4bf8
Fanny Dolisy <fdolisy@outlook.com>
2022-03-18 23:12:27 -0500

    small bugs

--- lexer.cpp
+++ lexer.cpp
@@ -9,59 +9,59 @@
 #include <fstream>
 
 // class used to simulate a DFA for this lexical analyzer
-class DFA 
+class DFA
 {
 
-    private:
-        // strings to represent certain character classes
-        std::string DIGITS_NO_ZERO = "123456789";
-        std::string LETTERS_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        std::string LETTERS_LOWER = "abcdefghijklmnopqrstuvwxyz";
+private:
+    // strings to represent certain character classes
+    std::string DIGITS_NO_ZERO = "123456789";
+    std::string LETTERS_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string LETTERS_LOWER = "abcdefghijklmnopqrstuvwxyz";
 
-        // int to represent current position if the token if a keyword or requires more than one character analysis
-        int keywordPost = 0;
+    // int to represent current position if the token if a keyword or requires more than one character analysis
+    int keywordPost = 0;
 
-    public:
-        
-        // the current char the DFA is reading
-        char currentChar;
-        // the current state the DFA is in
-        std::string officialState = "Start";
-        // vectors for both states that require a lookahead and tokens that have been accepted
-        std::vector<std::string> lookAheadStates, acceptedStates;
-        // the lexeme that may be printed out with a token
-        std::string lexeme = "";  
-        // are we at the end of a line
-        bool EOL = false;
+public:
 
-        //helper functions
-        bool isWhiteSpace(char);
-        bool containsChar(std::string, char);
-        void findWhichLookAhead();
-        bool checkSymbol(char);
-        void findNextState();
-        void acceptKeyWordState(std::string);
-        void rejectKeywordState();
+    // the current char the DFA is reading
+    char currentChar;
+    // the current state the DFA is in
+    std::string officialState = "Start";
+    // vectors for both states that require a lookahead and tokens that have been accepted
+    std::vector<std::string> lookAheadStates, acceptedStates;
+    // the lexeme that may be printed out with a token
+    std::string lexeme = "";
+    // are we at the end of a line
+    bool EOL = false;
 
-        // general states
-        void start(char);
-        void typeReference(char);
-        void identifier(char);
-        void assign(char);
-        void rangeSep(char);
-        void number(char);
-        
-        // keyword states
-        void BEGIN(char);
-        void DATE(char);
-        void TAGS(char);
-        void END(char);
-        void INTEGER(char);
-        void SEQUENCE(char);
-    
-        // lookahead states (other than keywords)
-        void hyphen(char);
-        void zero(char);
+    //helper functions
+    bool isWhiteSpace(char);
+    bool containsChar(std::string, char);
+    void findWhichLookAhead();
+    bool checkSymbol(char);
+    void findNextState();
+    void acceptKeyWordState(std::string);
+    void rejectKeywordState();
+
+    // general states
+    void start(char);
+    void typeReference(char);
+    void identifier(char);
+    void assign(char);
+    void rangeSep(char);
+    void number(char);
+
+    // keyword states
+    void BEGIN(char);
+    void DATE(char);
+    void TAGS(char);
+    void END(char);
+    void INTEGER(char);
+    void SEQUENCE(char);
+
+    // lookahead states (other than keywords)
+    void hyphen(char);
+    void zero(char);
 };
 
 // determines if the current character is a white space
@@ -126,6 +126,7 @@ bool DFA::checkSymbol(char currentChar)
 void DFA::acceptKeyWordState(std::string keyword)
 {
     acceptedStates.push_back(keyword);
+    lookAheadStates.clear();
     keywordPost = 0;
     officialState = "Start";
     lexeme = "";
@@ -135,7 +136,6 @@ void DFA::rejectKeywordState()
 {
     lookAheadStates.clear();
     lexeme += currentChar;
-    lookAheadStates.clear();
     keywordPost = 0;
 }
 // Start state for the DFA, looks at all possibilities for basic char
@@ -147,30 +147,30 @@ void DFA::start(char currentChar)
         // KEY WORDS
         switch (currentChar)
         {
-            case 'B':
-                lookAheadStates.push_back("BEGIN");
-                keywordPost++;
-                break;
-            case 'D':
-                lookAheadStates.push_back("DATE");
-                keywordPost++;
-                break;
-            case 'E':
-                lookAheadStates.push_back("END");
-                keywordPost++;
-                break;
-            case 'I':
-                lookAheadStates.push_back("INTEGER");
-                keywordPost++;
-                break;
-            case 'S':
-                lookAheadStates.push_back("SEQUENCE");
-                keywordPost++;
-                break;
-            case 'T':
-                lookAheadStates.push_back("TAGS");
-                keywordPost++;
-                break;
+        case 'B':
+            lookAheadStates.push_back("BEGIN");
+            keywordPost++;
+            break;
+        case 'D':
+            lookAheadStates.push_back("DATE");
+            keywordPost++;
+            break;
+        case 'E':
+            lookAheadStates.push_back("END");
+            keywordPost++;
+            break;
+        case 'I':
+            lookAheadStates.push_back("INTEGER");
+            keywordPost++;
+            break;
+        case 'S':
+            lookAheadStates.push_back("SEQUENCE");
+            keywordPost++;
+            break;
+        case 'T':
+            lookAheadStates.push_back("TAGS");
+            keywordPost++;
+            break;
         }
 
         // if not a keyword, it is  a typereference
@@ -180,7 +180,7 @@ void DFA::start(char currentChar)
     {
         // starts with a lower case letter
         officialState = "toIdentifier";
-        
+
     }
     else if (containsChar(DIGITS_NO_ZERO, currentChar))
     {
@@ -192,8 +192,8 @@ void DFA::start(char currentChar)
         // is a whitespace, ignore
         officialState = "Start";
     }
-    else 
-    { 
+    else
+    {
         // it is a symbol or bad input
         if (!checkSymbol(currentChar))
         {
@@ -209,8 +209,9 @@ void DFA::BEGIN(char currentChar)
     if ((isWhiteSpace(currentChar) && keywordPost == 5) || (EOL && currentChar == 'N' && keywordPost == 4))
     {
         acceptKeyWordState("BEGIN");
+        checkSymbol(currentChar);
     }
-    else if (currentChar == 'E' && keywordPost == 1 || currentChar == 'G' && keywordPost == 2 || currentChar == 'I' && keywordPost == 3|| currentChar == 'N' && keywordPost == 4)
+    else if (currentChar == 'E' && keywordPost == 1 || currentChar == 'G' && keywordPost == 2 || currentChar == 'I' && keywordPost == 3 || currentChar == 'N' && keywordPost == 4)
     {
         // continue processing word
         keywordPost++;
@@ -230,9 +231,10 @@ void DFA::BEGIN(char currentChar)
 void DFA::DATE(char currentChar)
 {
     // word is accepted  
-    if ((isWhiteSpace(currentChar) && keywordPost == 4) || (EOL && currentChar == 'E' && keywordPost == 3))
+    if ((isWhiteSpace(currentChar) && keywordPost == 4) || (EOL && currentChar == 'E' && keywordPost == 3) || lexeme == "DATE")
     {
         acceptKeyWordState("DATE");
+        checkSymbol(currentChar);
     }
     else if (currentChar == 'A' && keywordPost == 1 || currentChar == 'T' && keywordPost == 2 || currentChar == 'E' && keywordPost == 3)
     {
@@ -258,7 +260,9 @@ void DFA::TAGS(char currentChar)
     {
         //accept, no other possibility
         acceptKeyWordState("TAGS");
+        checkSymbol(currentChar);
     }
+
     else if (currentChar == 'A' && keywordPost == 1 || currentChar == 'G' && keywordPost == 2 || currentChar == 'S' && keywordPost == 3)
     {
         // continue processing word
@@ -282,6 +286,7 @@ void DFA::END(char currentChar)
     if (isWhiteSpace(currentChar) && keywordPost == 3 || (EOL && currentChar == 'D' && keywordPost == 2))
     {
         acceptKeyWordState("END");
+        checkSymbol(currentChar);
     }
     else if (currentChar == 'N' && keywordPost == 1 || currentChar == 'D' && keywordPost == 2)
     {
@@ -302,12 +307,13 @@ void DFA::END(char currentChar)
 // INTEGER keyword state for DFA
 void DFA::INTEGER(char currentChar)
 {
-   
+
     // word is accepted
     if (isWhiteSpace(currentChar) && keywordPost == 7 || (EOL && currentChar == 'R' && keywordPost == 6))
     {
         //accept, no other possibility
         acceptKeyWordState("INTEGER");
+        checkSymbol(currentChar);
     }
     else if (currentChar == 'N' && keywordPost == 1 || currentChar == 'T' && keywordPost == 2 || currentChar == 'E' && keywordPost == 3 ||
         currentChar == 'G' && keywordPost == 4 || currentChar == 'E' && keywordPost == 5 || currentChar == 'R' && keywordPost == 6)
@@ -329,16 +335,17 @@ void DFA::INTEGER(char currentChar)
 // SEQUENCE keyword state for DFA
 void DFA::SEQUENCE(char currentChar)
 {
-    
+
     // word is accepted
     if (isWhiteSpace(currentChar) && keywordPost == 8 || (EOL && currentChar == 'E' && keywordPost == 7))
     {
         //accept, no other possibility
         acceptKeyWordState("SEQUENCE");
+        checkSymbol(currentChar);
     }
     else if (currentChar == 'E' && keywordPost == 1 || currentChar == 'Q' && keywordPost == 2 || currentChar == 'U' && keywordPost == 3 ||
-    currentChar == 'E' && keywordPost == 4 || currentChar == 'N' && keywordPost == 5 || currentChar == 'C' && keywordPost == 6 ||
-    currentChar == 'E' && keywordPost == 7)
+        currentChar == 'E' && keywordPost == 4 || currentChar == 'N' && keywordPost == 5 || currentChar == 'C' && keywordPost == 6 ||
+        currentChar == 'E' && keywordPost == 7)
     {
         // continue processing word
         keywordPost++;
@@ -388,7 +395,7 @@ void DFA::number(char currentChar)
             checkSymbol(currentChar);
             officialState = "Start";
         }
-      
+
     }
     else
     {
@@ -413,7 +420,7 @@ void DFA::typeReference(char currentChar)
     if (currentChar == '-')
     {
         // ensure the hyphen is acceptable
-    lookAheadStates.push_back("hyphen");
+        lookAheadStates.push_back("hyphen");
     }
     // if end of line, accept the type reference
     else if (EOL)
@@ -425,9 +432,9 @@ void DFA::typeReference(char currentChar)
         }
         checkSymbol(currentChar);
         officialState = "Start";
-        
+
     }
-    else if(isalnum(currentChar))
+    else if (isalnum(currentChar))
     {
         // continue processing typereference
         officialState = "toTypeRef";
@@ -438,13 +445,13 @@ void DFA::typeReference(char currentChar)
         //accept type reference
         acceptedStates.push_back("typereference");
         officialState = "Start";
-        
+
     }
     else
     {
         officialState = "fail";
     }
-    
+
 
 }
 // state for an identifier
@@ -460,7 +467,7 @@ void DFA::identifier(char currentChar)
         }
         checkSymbol(currentChar);
         officialState = "Start";
-        
+
     }
     else if (isalnum(currentChar))
     {
@@ -478,7 +485,7 @@ void DFA::identifier(char currentChar)
         // accept
         acceptedStates.push_back("identifier");
         officialState = "Start";
-        
+
     }
     else
     {
@@ -519,7 +526,7 @@ void DFA::assign(char currentChar)
 // state for a range seperator
 void DFA::rangeSep(char currentChar)
 {
- 
+
     if (currentChar == '.' && keywordPost == 0 || currentChar == '.' && keywordPost == 1)
     {
         // continue processing input
@@ -585,6 +592,10 @@ void DFA::findWhichLookAhead()
         {
             zero(currentChar);
         }
+        else if (lookAheadStates[z] == "Start")
+        {
+            start(currentChar);
+        }
     }
 }
 // helper function to process next state
@@ -621,7 +632,7 @@ int main()
     DFA dfa;
     std::ifstream MyReadFile("input.txt");
     std::string input = "";
-    while (std::getline(MyReadFile, input)) 
+    while (std::getline(MyReadFile, input))
     {
         // Output the text from the file
         for (int i = 0; i < input.length(); i++)
@@ -637,14 +648,14 @@ int main()
             {
                 dfa.EOL = false;
             }
-            
+
 
             // if no look ahead states, go to official ones
             if (!dfa.lookAheadStates.empty())
             {
                 dfa.findWhichLookAhead();
             }
-            else 
+            else
             {
                 dfa.findNextState();
             }
@@ -654,7 +665,7 @@ int main()
             {
                 for (int i = 0; i < dfa.acceptedStates.size(); i++)
                 {
-                    if(dfa.acceptedStates[i] == "typereference" || dfa.acceptedStates[i] == "identifier" || dfa.acceptedStates[i] == "Number")
+                    if (dfa.acceptedStates[i] == "typereference" || dfa.acceptedStates[i] == "identifier" || dfa.acceptedStates[i] == "Number")
                     {
                         std::cout << "<" << dfa.acceptedStates[i] << "> " << "  <" << dfa.lexeme << "> " << std::endl;
                         dfa.lexeme = "";
@@ -677,3 +688,7 @@ int main()
         }
     }
 }
+
+
+
+
